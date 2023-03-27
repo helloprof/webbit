c20d4acb7b498e3cd4979fa9a38ec2c62529f050
helloprof <suluxan.seneca@gmail.com>
2023-03-27 13:34:48 -0400

    onsubmit event for form

--- src.js
+++ src.js
@@ -1,1 +1,13 @@
 console.log("HELLO FROM SRC!!!")
+
+
+var form = document.querySelector("#post-form")
+console.log(form)
+
+
+var postTitle = document.querySelector("#title").value
+
+form.onsubmit = function() {
+    console.log(postTitle)
+}
+
